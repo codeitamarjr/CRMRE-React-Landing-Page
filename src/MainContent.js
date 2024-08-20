696eb2b45a48b226a0e2866bd13effe15767107f
Itamar Junior <hello@itjunior.dev>
2024-08-20 23:05:27 +0100

    chore: Update npm dependencies and improve code readability

--- src/MainContent.js
+++ src/MainContent.js
@@ -108,11 +108,11 @@ function MainContent() {
                     <div className='mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8'>
                         <div className='relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0'>
                             <svg viewBox='0 0 1024 1024' className='absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0' aria-hidden='true'>
-                                <circle cx='512' cy='512' r='512' fill='url(#759c1415-0410-454c-8f7c-9a820de03641)' fill-opacity='0.7' />
+                                <circle cx='512' cy='512' r='512' fill='url(#759c1415-0410-454c-8f7c-9a820de03641)' fillOpacity='0.7' />
                                 <defs>
                                     <radialGradient id='759c1415-0410-454c-8f7c-9a820de03641'>
-                                        <stop stop-color='#7775D6' />
-                                        <stop offset='1' stop-color='#E935C1' />
+                                        <stop stopColor='#7775D6' />
+                                        <stop offset='1' stopColor='#E935C1' />
                                     </radialGradient>
                                 </defs>
                             </svg>
